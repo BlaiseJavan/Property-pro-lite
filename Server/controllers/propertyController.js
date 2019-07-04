bf2bb86cd09036212ea6a1d6814a041f11c33edf
BlaiseJavan <blaiseirakoze50@gmail.com>
2019-07-04 12:52:11 +0200

    [ft-167041581] Post advert

--- Server/controllers/propertyController.js
+++ Server/controllers/propertyController.js
@@ -2,7 +2,6 @@ import moment from 'moment';
 import Property from '../db/property';
 
 class propertyController {
-  
   static createProperty(req, res) {
     const newId = parseInt(Property.length) + 1;
     const {
@@ -22,6 +21,24 @@ class propertyController {
     };
     Property.push(newProperty);
     return res.status(200).json({status: 'success', newProperty });
+  }
+
+  static updateProperty(req, res) {
+    const { id } = req.params;
+    const property = Property.find(updateProperty => updateProperty.id == id);
+    if (property) {
+      (property.owner = req.body.owner), (property.price = req.body.price), 
+      (property.status = req.body.status), (property.city = req.body.city),
+      (property.state = req.body.state), (property.address = req.body.address),
+      (property.type = req.body.type), (property.created_on = new Date()),
+      (property.imageUrl = req.body.imageUrl);   
+      return res.status(200).json({
+        status: 'success', updateProperty: property,
+      });
+    }
+    res.status(400).json({
+      error: 'Property cannot be updated',
+    });
   }
 }
 
